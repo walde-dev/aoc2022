9934d0b3a114c78da6986acdf6ceed488ffb5786
walde-dev <paninwaldemar@gmail.com>
2022-12-02 00:32:15 +0100

    day1

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,11 +1,29 @@
 import Puzzle from '../../types/AbstractPuzzle';
 
 export default class ConcretePuzzle extends Puzzle {
+  public parseInput(input: string): number[][] {
+    return input
+      .split('\n\n')
+      .map((elf) => elf.split(' '))
+      .map((elf) => elf[0].split(`\n`).map((calories) => parseInt(calories)))
+      .sort((a, b) => {
+        return (
+          b.reduce((acc, curr) => acc + curr, 0) -
+          a.reduce((acc, curr) => acc + curr, 0)
+        );
+      });
+  }
   public solveFirst(): string {
-    return 'day 1 solution 1';
+    return this.parseInput(this.input)[0]
+      .reduce((acc, curr) => acc + curr, 0)
+      .toString();
   }
   public solveSecond(): string {
-    return 'day 1 solution 2';
+    return this.parseInput(this.input)
+      .slice(0, 3)
+      .map((elv) => elv.reduce((acc, curr) => acc + curr, 0))
+      .reduce((acc, curr) => acc + curr, 0)
+      .toString();
   }
 
   public getFirstExpectedResult(): string {
